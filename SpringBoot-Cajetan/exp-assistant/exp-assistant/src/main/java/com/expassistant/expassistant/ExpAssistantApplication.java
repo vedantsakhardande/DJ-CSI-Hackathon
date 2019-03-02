155899b1bcb364f22740bb93d35e68de52ec1633
Unknown <rigrod1999@gmail.com>
2019-03-02 10:15:36 +0530

    v 1.0 - Template created

---
+++ SpringBoot-Cajetan/exp-assistant/exp-assistant/src/main/java/com/expassistant/expassistant/ExpAssistantApplication.java
@@ -0,0 +1,13 @@
+package com.expassistant.expassistant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpAssistantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpAssistantApplication.class, args);
+	}
+
+}
